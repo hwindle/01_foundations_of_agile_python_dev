c18f1344cb63137a1b55b9e499268ab08ec17c45
Hazel Windle <hwindle83@googlemail.com>
2025-06-24 18:32:03 +0100

    Finished abruptly goblins game night python script

--- codecademy/Abruptly_Goblins/main.py
+++ codecademy/Abruptly_Goblins/main.py
@@ -13,7 +13,7 @@ def add_gamer(gamer, gamers_list):
     gamers_list.append(gamer)
 
 kimberly = {'name': 'Kimberly Warner', 
-            'availability': ['Mondays', 'Tuesdays', 'Fridays'] }  
+            'availability': ['Monday', 'Tuesday', 'Friday'] }  
 
 add_gamer(kimberly, gamers)
 add_gamer({'name':'Thomas Nelson','availability': ["Tuesday", "Thursday", "Saturday"]}, gamers)
@@ -26,4 +26,25 @@ add_gamer({'name':'Crystal Brewer','availability': ["Thursday", "Friday", "Satur
 add_gamer({'name':'James Barnes Jr.','availability': ["Tuesday", "Wednesday", "Thursday", "Sunday"]}, gamers)
 add_gamer({'name':'Michel Trujillo','availability': ["Monday", "Tuesday", "Wednesday"]}, gamers)
 
-print(gamers)
+
+def build_daily_frequency_table():
+    return {'Monday': 0, 'Tuesday': 0, 
+            'Wednesday': 0, 'Thursday': 0, 
+            'Friday': 0, 'Saturday': 0, 'Sunday': 0 }
+
+count_availability = build_daily_frequency_table()
+
+def calculate_availability(gamers_list, available_frequency):
+    for gamer in gamers_list:
+        for day in gamer['availability']:
+            available_frequency[day] += 1
+    return available_frequency
+
+results_frequency = calculate_availability(gamers, count_availability)
+print(results_frequency)    
+
+def find_best_night(availability_table):
+    return max(availability_table.items(), key=lambda x: x[1])
+
+game_night = find_best_night(results_frequency)
+print(f'Games night is: {game_night[0]}.')
