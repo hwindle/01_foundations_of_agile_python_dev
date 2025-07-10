19ad11809ded800ec4a10662e5c8e77b16e26ff9
Hazel Windle <hwindle83@googlemail.com>
2025-07-10 20:16:02 +0100

    Find depth of binary search tree dict. function

---
+++ codecademy/computer_science/find_depth_of_binary_search_tree.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+def depth(tree):
+    if not tree:
+        return 0
+    left_depth = depth(tree['left_child'])
+    right_depth = depth(tree['right_child'])
+    if left_depth > right_depth:
+        return left_depth + 1
+    else:
+        return right_depth + 1
+
+
+# HELPER FUNCTION TO BUILD TREES
+def build_bst(bst_list):
+    if len(bst_list) == 0:
+        return None
+
+    mid_idx = len(bst_list) // 2
+    mid_val = bst_list[mid_idx]
+
+    tree_node = {'data': mid_val}
+    tree_node['left_child'] = build_bst(bst_list[:mid_idx])
+    tree_node['right_child'] = build_bst(bst_list[mid_idx + 1 :])
+    return tree_node
+
+
+
+
+# HELPER VARIABLES
+tree_level_1 = build_bst([1])
+tree_level_2 = build_bst([1, 2, 3])
+tree_level_4 = build_bst([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]) 
+
+# test cases
+print(depth(tree_level_1) == 1)
+print(depth(tree_level_2) == 2)
+print(depth(tree_level_4) == 4)
