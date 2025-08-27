6f6eb75eca85c0815e034e401998556725026c50
Hazel Windle <hwindle83@googlemail.com>
2025-08-27 18:00:34 +0100

    Added child path to queue in BFS

--- codecademy/computer_science/trees/breadth_first_tree_search.py
+++ codecademy/computer_science/trees/breadth_first_tree_search.py
@@ -5,11 +5,16 @@ def bfs(root_node, goal_value):
     initial_path = [root_node]
     path_queue.appendleft(initial_path)
   
-# Implement search loop below
-while len(path_queue) > 0:
-    current_path = path_queue.pop()
-    current_node = current_path[-1]
-    print(f'Output string {current_node.value}')
-    if current_node.value == goal_value:
-        return current_path
-    return None
+    # Implement search loop below
+    while len(path_queue) > 0:
+        current_path = path_queue.pop()
+        current_node = current_path[-1]
+        print(f'Output string {current_node.value}')
+        if current_node.value == goal_value:
+            return current_path  
+        # add paths to children to the  frontier
+        for child in current_node.children:
+            new_path = current_path[:]
+            new_path.append(child)
+            path_queue.appendleft(new_path)
+        return None
